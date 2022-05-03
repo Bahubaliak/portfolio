67c6f5abf8cc2891f5b8aa7ab08b6a448b53e3ab
bahubali <bahubali@webrino.com>
2022-05-03 10:26:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row text-center">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
